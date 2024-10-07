dfaf92a49010bb9e8ecdc7e1cb1130dc53cc2b7e
Берсенев Максим <bersenev.maks@gmail.com>
2024-10-07 20:51:38 +0500

    1

---
+++ MakeProtocols/Automat.cs
@@ -0,0 +1,21 @@
+﻿namespace MakeProtocols
+{
+    public class Automat
+    {
+        public string NameAutomat { get; set; }
+        public string Description { get; set; }
+        public string Type { get; set; }
+        public string NumbVendor { get; set; }
+        public string NominalCurrent { get; set; }
+        public string NominalVoltage { get; set; }
+        public string Ust_Ir { get; set; }
+        public string Ust_Tr { get; set; }
+        public string Ust_Isd { get; set; }
+        public string Ust_Tsd { get; set; }
+        public string Ust_Ii { get; set; }
+        public string Ust_Ti { get; set; }
+        public string Ust_Ig { get; set; }
+        public string Ust_Tg { get; set; }
+
+    }
+}
