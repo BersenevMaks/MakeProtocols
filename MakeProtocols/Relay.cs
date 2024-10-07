e116df66f57cabe13debf743bce504d0b46fa8d2
Берсенев Максим <bersenev.maks@gmail.com>
2024-10-07 21:08:16 +0500

    2

---
+++ MakeProtocols/Relay.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MakeProtocols
+{
+    public class Relay
+    {
+        public string IDrelay { get; set; }
+        public string TypeRelay { get; set; }
+    }
+}
