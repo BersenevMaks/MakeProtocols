e540cee71e14084502cb5af4562e10a97535b74a
Берсенев Максим <bersenev.maks@gmail.com>
2024-10-08 19:21:56 +0500

    Add SF

---
+++ MakeProtocols/SF.cs
@@ -0,0 +1,15 @@
+﻿namespace MakeProtocols
+{
+    public abstract class SF
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string Character { get; set; }
+        public string Inom { get; set; }
+        public string Phase { get; set; }
+        public string Ioverload { get; set; }
+        public string Toverload { get; set; }
+        public string Ito { get; set; }
+        public string Tto { get; set; }
+    }
+}
